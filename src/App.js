0863eb319632fe9fe4f74b2cc771bf3f0ea3a4f8
Peter Camerini <Camerini.peter@gmail.com>
2018-05-01 23:16:49 +0200

    j

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ let titleStyle = {
   "font-size": "54px"
 }
 
+let fakeServerData = {
+  user:{
+    name: "Peter",
+  }
+}
 
 class Aggregate extends Component{
   render() {
@@ -26,7 +31,7 @@ class Filter extends Component{
   render(){
     return(
       <div style={defaultStyle} >
-        <img/>
+       
         <input type="text"/>
       </div>  
     );
@@ -37,7 +42,7 @@ class Playlist extends Component {
   render() {
     return(
       <div style={{...defaultStyle, width: "25%", display: "inline-block"}}>
-        <img/>
+      
         <h3>Playlist Name</h3>
         <ul><p>Song 1</p><p>Song 1</p><p>Song 1</p></ul>
       </div>
@@ -46,10 +51,18 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    this.setState({serverData: fakeServerData});
+  }
   render() {
     return (
       <div className="App" style={defaultStyle}>      
-        <h1 style={{...titleStyle}}>Title</h1>
+        <h1 style={{...titleStyle}}>
+        {this.state.serverData && this.state.serverData.user.name}'s playlist</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
